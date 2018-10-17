757f50dcc989bec06b548a7cb6cc81ffd6b4b79e
sutharkamlesh <kamlesh08@live.com>
2018-10-17 12:04:52 +0530

    merge all intents in one UDF

--- webhook.py
+++ webhook.py
@@ -20,12 +20,7 @@ def webhook():
     req = request.get_json(silent=True, force=True)
     #print(json.dumps(req, indent=4))
 
-    if req.get("result").get("action") == "getcontact":
-        res = get_contact(req)
-    elif req.get("result").get("action") == "givingAddress":
-        res = get_address(req)
-    elif req.get("result").get("action") == "getPolicy":
-        res = get_policy(req)
+    res = processRequest(req)
 
     res = json.dumps(res, indent=4)
     # print(res)
@@ -34,46 +29,50 @@ def webhook():
     return r
     #return	data.to_json()
 
-def get_address(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    address = data[data["state"]==parameters["state"]][data["type"] == parameters["type"]].to_string()
-    if address.split()[0] != 'Empty':
-        speech = "Here is the address: "+data[data["state"]==parameters["state"]][data["type"] == parameters["type"]]['address'].to_string()[1:]
-    else:
-        speech = "Sorry we don't have this information"
-    return  {
-    "speech": speech,
-    "displayText": speech,
-    "source": "webhook"
-    }
 
-def get_contact(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    address = data[data["state"]==parameters["state"]][data["type"] == parameters["type"]].to_string()
-    if address.split()[0] != 'Empty':
-        speech = "Here it is: "+data[data["state"]==parameters["state"]][data["type"] == parameters["type"]]['phone'].to_string()[1:]
-    else:
-        speech = "Sorry we don't have this information"
-    return {
-    "speech": speech,
-    "displayText": speech,
-    "source": "webhook"
-    }
+def processRequest(req):
+	if req.get("result").get("action") == "getcontact":
+        result = req.get("result")
+	    parameters = result.get("parameters")
+	    address = data[data["state"]==parameters["state"]][data["type"] == parameters["type"]].to_string()
+	    if address.split()[0] != 'Empty':
+	        speech = "Here it is: "+data[data["state"]==parameters["state"]][data["type"] == parameters["type"]]['phone'].to_string()[1:]
+	    else:
+	        speech = "Sorry we don't have this information"
+	    return {
+	    "speech": speech,
+	    "displayText": speech,
+	    "source": "webhook"
+	    }
+    elif req.get("result").get("action") == "givingAddress":
+        result = req.get("result")
+	    parameters = result.get("parameters")
+	    address = data[data["state"]==parameters["state"]][data["type"] == parameters["type"]].to_string()
+	    if address.split()[0] != 'Empty':
+	        speech = "Here is the address: "+data[data["state"]==parameters["state"]][data["type"] == parameters["type"]]['address'].to_string()[1:]
+	    else:
+	        speech = "Sorry we don't have this information"
+	    return  {
+	    "speech": speech,
+	    "displayText": speech,
+	    "source": "webhook"
+	    }
+    elif req.get("result").get("action") == "getPolicy":
+        result = req.get("result")
+	    parameters = result.get("parameters")
+	    if parameters['policy'] in policy.keys():
+	        speech = "You can see our "+ parameters['policy'] +" policy from here: " + policy[parameters['policy']]
+	    else:
+	        speech = "Sorry we don't have this information"
+	    return {
+	    "speech": speech,
+	    "displayText": speech,
+	    "source": "webhook"
+	    }
+	 else:
+	 	return {}
 
-def get_policy(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    if parameters['policy'] in policy.keys():
-        speech = "You can see our "+ parameters['policy'] +" policy from here: " + policy[parameters['policy']]
-    else:
-        speech = "Sorry we don't have this information"
-    return {
-    "speech": speech,
-    "displayText": speech,
-    "source": "webhook"
-    }
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port {}".format(port))
